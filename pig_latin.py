d409358ee5acd90879f8d76ce56502bc1312405e
iamali-1 <syedmuhammadalihaider01@gmail.com>
2026-05-12 17:39:38 +0500

    created a pig latin sentence function

--- pig_latin.py
+++ pig_latin.py
@@ -16,15 +16,34 @@
 # print(pig_latin("Python"))
 
 
-def alternate_pig_latin(word: str) -> str:
-    vowels = {"a", "e", "i", "o", "u"}
-    found_vowels = [letter for letter in word.lower() if letter in vowels]
+# def alternate_pig_latin(word: str) -> str:
+#     vowels = {"a", "e", "i", "o", "u"}
+#     found_vowels = [letter for letter in word.lower() if letter in vowels]
 
-    if len(found_vowels) >= 2:
-        return word + "way"
+#     if len(found_vowels) >= 2:
+#         return word + "way"
 
-    return word[1:] + word[0] + "ay"
+#     return word[1:] + word[0] + "ay"
+
+# print(alternate_pig_latin("wine"))
 
 
+def pl_sentence(sentence: str) -> str:
+    vowels = ["a", "e", "i", "o", "u"]
+    new_word = None
+    splitted = sentence.split(" ")
+    new_splitted = []
 
-print(alternate_pig_latin("wine"))
+    for word in splitted:
+        w = word[0]
+        if w in vowels:
+            new_word = word + "way"
+            new_splitted.append(new_word)
+        elif w not in vowels:
+            new_word = word[1:] + w + "ay"
+            new_splitted.append(new_word)
+    new_string = " ".join(new_splitted)
+    return new_string
+
+
+print(pl_sentence("this is a test translation"))
