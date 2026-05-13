87f05189d7c43b9edc0565af1452f10faf8562c2
iamali-1 <syedmuhammadalihaider01@gmail.com>
2026-05-13 15:07:13 +0500

    sorted text

--- strsort.py
+++ strsort.py
@@ -1,9 +1,21 @@
-def strsort(word: str) -> str:
-    sorted = []
-    for letter in word:
-        sorted.append(letter)
-    sorted.sort()
-    return "".join(sorted)
+# def strsort(word: str) -> str:
+#     sorted = []
+#     for letter in word:
+#         sorted.append(letter)
+#     sorted.sort()
+#     return "".join(sorted)
 
 
-print(strsort("octopus"))
+# print(strsort("octopus"))
+
+def strsort(text: str) -> str:
+    result = []
+    words = text.lower().split(" ")
+    
+    for word in words:
+        sorted_word = "".join(sorted(word)).capitalize()
+        result.append(sorted_word)
+
+    return ", ".join(result)
+
+print(strsort("Tom Dick Harry"))
