d01650253c9614bb50ac65d98c82de56419e42d8
iamali-1 <syedmuhammadalihaider01@gmail.com>
2026-05-15 18:00:28 +0500

    created a sorted_countries function

--- firstlast.py
+++ firstlast.py
@@ -78,33 +78,32 @@
 # )
 
 
-# def sorted_countries(items):
-#     for item in items:
-#         if isinstance(item, dict):
-#             for key, value in item.items():
-#                 print(key, value)
+# def lengthOfLastWord(s: str) -> str:
+#     s = s.strip().split()
+#     return len(s[-1])
 
 
-# print(
-#     sorted_countries(
-#         [
-#             {"name": "Canada", "size": 9984670, "population": 38250000},
-#             {"name": "Italy", "size": 301340, "population": 59110000},
-#             {"name": "United Kingdom", "size": 242495, "population": 67220000},
-#             {"name": "France", "size": 551695, "population": 67390000},
-#             {"name": "Germany", "size": 357022, "population": 83200000},
-#             {"name": "Japan", "size": 377975, "population": 125700000},
-#             {"name": "United States", "size": 9833517, "population": 331900000},
-#         ]
-#     )
-# )
+# print(lengthOfLastWord("luffy is still joyboy"))
+# print(lengthOfLastWord("Hello World"))
+# print(lengthOfLastWord("   fly me   to   the moon  "))
+
+import operator
+from pprint import pprint
+
+def sorted_countries(items):
+    return (sorted(items,key=operator.itemgetter('name')))
 
 
-def lengthOfLastWord(s: str) -> str:
-    s = s.strip().split()
-    return len(s[-1])
-
-
-print(lengthOfLastWord("luffy is still joyboy"))
-print(lengthOfLastWord("Hello World"))
-print(lengthOfLastWord("   fly me   to   the moon  "))
+pprint(
+    sorted_countries(
+        [
+            {"name": "Canada", "size": 9984670, "population": 38250000},
+            {"name": "Italy", "size": 301340, "population": 59110000},
+            {"name": "United Kingdom", "size": 242495, "population": 67220000},
+            {"name": "France", "size": 551695, "population": 67390000},
+            {"name": "Germany", "size": 357022, "population": 83200000},
+            {"name": "Japan", "size": 377975, "population": 125700000},
+            {"name": "United States", "size": 9833517, "population": 331900000},
+        ]
+    )
+)
