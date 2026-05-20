384f7aeaefe24a66d8a2ee76e01a3afc043f5484
iamali-1 <syedmuhammadalihaider01@gmail.com>
2026-05-20 12:27:57 +0500

    added dictduff function

--- login.py
+++ login.py
@@ -68,32 +68,45 @@
 
 # print(family())
 
-from pprint import pprint
+# from pprint import pprint
 
 
-def get_rainfall():
-    rainfall_data = {}
+# def get_rainfall():
+#     rainfall_data = {}
 
-    while True:
-        city = input("Enter City Name: ").strip()
+#     while True:
+#         city = input("Enter City Name: ").strip()
 
-        if city == "":
-            break
-        while True:
-            rainfall_input = (input("Enter Rainfall: ")).strip()
-            if rainfall_input.isdigit():
-                rainfall = int(rainfall_input)
-                break
-            else:
-                print("Invalid Input. Numbers Only.")
+#         if city == "":
+#             break
+#         while True:
+#             rainfall_input = (input("Enter Rainfall: ")).strip()
+#             if rainfall_input.isdigit():
+#                 rainfall = int(rainfall_input)
+#                 break
+#             else:
+#                 print("Invalid Input. Numbers Only.")
 
-        if city in rainfall_data:
-            rainfall_data[city] += rainfall
-        else:
-            rainfall_data[city] = rainfall
+#         if city in rainfall_data:
+#             rainfall_data[city] += rainfall
+#         else:
+#             rainfall_data[city] = rainfall
 
-    for key, value in rainfall_data.items():
-        pprint(f"City: {key}, Rainfall: {value}mm.")
+#     for key, value in rainfall_data.items():
+#         pprint(f"City: {key}, Rainfall: {value}mm.")
 
 
-print(get_rainfall())
+# print(get_rainfall())
+
+
+def dictduff(d1, d2):
+    final = {}
+    all_keys = set(d1.keys()).union(d2.keys())
+    print(all_keys)
+    for key in all_keys:
+        if d1.get(key) != d2.get(key):
+            final[key] = [d1.get(key), d2.get(key)]
+    return final
+
+
+print(dictduff({"a": 1, "b": 2, "c": 3}, {"a": 1, "b": 2, "c": 4}))
